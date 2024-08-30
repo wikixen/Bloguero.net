615a2184c1abc88b75d90ce01789bd69266d82a5
wikixen <bennylopezhernandez0@gmail.com>
2024-08-30 18:54:35 -0400

    Remade go.sum & go.mod as well as adding some code to main.go

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/glebarez/sqlite"
+	_ "github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+type Blogs struct {
+	gorm.Model
+	Title   string
+	Author  string
+	Content string
+}
+
+var db, err = gorm.Open(sqlite.Open("blogs.db"), &gorm.Config{})
 
 func main() {
-	fmt.Println("Hello World")
+	db.AutoMigrate(&Blogs{})
+
 }
