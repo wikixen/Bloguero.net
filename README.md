d5a31cceace8955b1fc4148167eedcca7184d7ec
wikixen <bennylopezhernandez0@gmail.com>
2024-08-30 22:30:21 -0400

    Fixed returns on handlefuncs & added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+TODO
+- [ ] Need to finish my routes
+- [ ] Make it thread safe??(Look at Flo Woelki's youtube vid if that doesnt make sense)
+- [ ] Separate main into diff files
--- main.go
+++ main.go
@@ -56,23 +56,33 @@ func CreateBlog(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func GetBlogs(w http.ResponseWriter, r *http.Request) []Blogs {
+func GetBlogs(w http.ResponseWriter, r *http.Request) {
 	var allBlogs []Blogs
 	if res := db.Find(&allBlogs); res.Error != nil {
 		http.Error(w, res.Error.Error(), http.StatusInternalServerError)
 	} else {
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		j, err := json.Marshal(allBlogs)
+		if err != nil {
+			http.Error(w, res.Error.Error(), http.StatusInternalServerError)
+		}
+		w.Write(j)
 	}
-	return allBlogs
 }
 
-func GetABlog(w http.ResponseWriter, r *http.Request) Blogs {
+func GetABlog(w http.ResponseWriter, r *http.Request) {
 	var blog Blogs
-	id := r.URL.Query().Get("id")
+	id := r.PathValue("id")
 	if res := db.First(&blog, "id = ?", id); res.Error != nil {
 		http.Error(w, res.Error.Error(), http.StatusInternalServerError)
 	} else {
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		j, err := json.Marshal(blog)
+		if err != nil {
+			http.Error(w, res.Error.Error(), http.StatusInternalServerError)
+		}
+		w.Write(j)
 	}
-	return blog
 }
